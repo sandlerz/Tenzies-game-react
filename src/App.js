8a9ee6af2f51c1bf75a8249a785a6988605ca181
Sebastian Mesa <sandlerzop@gmail.com>
2022-04-22 20:57:51 -0500

    new feature add (time tracking)

--- src/App.js
+++ src/App.js
@@ -1,72 +1,83 @@
-import { useState, useEffect } from "react";
-import { nanoid } from "nanoid";
+import { useState, useEffect } from "react"
+import { nanoid } from "nanoid"
 
-import Die from "./Components/Die";
+import Die from "./Components/Die"
 
-import "./Styles/style.css";
+import "./Styles/style.css"
+
+function allNewDice() {
+  let arrDice = []
+  for (let i = 0; i < 10; i++) {
+    let ranNum = Math.ceil(Math.random() * 6)
+    arrDice.push({
+      value: ranNum,
+      isHeld: false,
+      id: nanoid(),
+    })
+  }
+  return arrDice
+}
 
 export default function App() {
-  const [dice, setDice] = useState(allNewDice());
-  const [tenzies, setTenzies] = useState(false);
-  const [counter, setCounter] = useState(0);
-  const [prevCounter, setPrevCounter] = useState(0);
+  const [dice, setDice] = useState([])
+  const [tenzies, setTenzies] = useState(false)
+  const [counter, setCounter] = useState(0)
+  const [prevCounter, setPrevCounter] = useState(0)
+  const [time, setTime] = useState({ time: 0, lastTime: 0, stop: true })
 
   useEffect(() => {
-    const wonHeld = dice.every((die) => die.isHeld);
-    const wonValue = dice.every((die) => die.value === dice[0].value);
+    const wonHeld = dice.every(die => die.isHeld)
+    const wonValue = dice.every(die => die.value === dice[0].value)
     if (wonHeld && wonValue) {
-      setTenzies(true);
-      setTime(time)
+      setTenzies(true)
+      setTime(prev => ({ ...prev, stop: true }))
     }
-  }, [dice]);
+  }, [dice])
 
-  function allNewDice() {
-    let arrDice = [];
-    for (let i = 0; i < 10; i++) {
-      let ranNum = Math.ceil(Math.random() * 6);
-      arrDice.push({
-        value: ranNum,
-        isHeld: false,
-        id: nanoid(),
-      });
-    }
-    return arrDice;
-  }
+  useEffect(() => {
+    if (time.stop) return
+    const intervalId = setInterval(() => {
+      setTime(prev => ({ ...prev, time: prev.time + 1 }))
+    }, 1000)
+
+    return () => clearInterval(intervalId)
+  }, [time.stop])
 
   const Roll = () => {
     if (tenzies) {
-      setDice(allNewDice());
-      setTenzies(false);
-      setPrevCounter(counter);
-      setCounter(-1);
-      setTime(0)
+      setPrevCounter(counter)
+      setTime(prev => ({ time: 0, lastTime: prev.time }))
+      setCounter(-1)
+      setDice(allNewDice())
+      setTenzies(false)
     } else {
-      setDice((prev) =>
-        prev.map((die) =>
+      setDice(prev =>
+        prev.map(die =>
           die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
         )
-      );
+      )
+      setTime(prev => ({ ...prev, stop: false }))
     }
-  };
+  }
 
   const handleCount = () => {
-    setCounter((prev) => prev + 1);
-  };
+    setCounter(prev => prev + 1)
+  }
 
-  const holdDice = (id) => {
-    setDice((prev) =>
-      prev.map((die) => (die.id === id ? { ...die, isHeld: !die.isHeld } : die))
-    );
-  };
+  const holdDice = id => {
+    setDice(prev =>
+      prev.map(die => (die.id === id ? { ...die, isHeld: !die.isHeld } : die))
+    )
+  }
 
-  const diceElements = dice.map((die) => (
+  const diceElements = dice.map(die => (
     <Die
       key={die.id}
       value={die.value}
       onClick={() => holdDice(die.id)}
       isHeld={die.isHeld}
     />
-  ));
+  ))
 
   return (
     <main>
@@ -77,28 +88,34 @@ export default function App() {
           current value between rolls.
         </p>
       </div>
+
       <div className="die-container">{diceElements}</div>
 
-      <div className="footer">
-        <div className="last">
-          <h2 className="rollTitle">Pre Roll</h2>
-          <p className="counter">{prevCounter}</p>
+      <div className="dashboard">
+        <div className="count__container">
+          <div className="last">
+            <h2 className="rollTitle">Pre Roll</h2>
+            <p className="counter">{prevCounter}</p>
+          </div>
+          <button
+            className="button"
+            onClick={() => {
+              Roll()
+              handleCount()
+            }}
+          >
+            {tenzies ? "New Game" : "Roll"}
+          </button>
+          <div className="next">
+            <h2 className="rollTitle">Roll</h2>
+            <p className="counter">{counter}</p>
+          </div>
         </div>
-        <button
-          className="button"
-          onClick={() => {
-            Roll();
-            handleCount();
-          }}
-        >
-          {tenzies ? "New Game" : "Roll"}
-        </button>
-        <div className="next">
-          <h2 className="rollTitle">Roll</h2>
-          <p className="counter">{counter}</p>
+        <div className="timer">
+          <p className="time">Time: {time.time}</p>
+          <p className="lastTime">Last Time: {time.lastTime}</p>
         </div>
       </div>
-
     </main>
-  );
+  )
 }
--- src/Components/Die.jsx
+++ src/Components/Die.jsx
@@ -1,14 +1,13 @@
-import dice1 from "../images/dice1.png";
-import dice2 from "../images/dice2.png";
-import dice3 from "../images/dice3.png";
-import dice4 from "../images/dice4.png";
-import dice5 from "../images/dice5.png";
-import dice6 from "../images/dice6.png";
+import dice1 from "../images/dice1.png"
+import dice2 from "../images/dice2.png"
+import dice3 from "../images/dice3.png"
+import dice4 from "../images/dice4.png"
+import dice5 from "../images/dice5.png"
+import dice6 from "../images/dice6.png"
 
 const dices = [dice1, dice2, dice3, dice4, dice5, dice6]
 
 export default function Die({ value, isHeld, onClick }) {
-  console.log(value)
   return (
     <div
       className="dice"
@@ -17,5 +16,5 @@ export default function Die({ value, isHeld, onClick }) {
     >
       <img className="dice-img" src={dices[value - 1]} alt="" />
     </div>
-  );
+  )
 }
